15daac89daf2851d7c568bef787c1290efb1e6a1
ExecutedPoorly <Executedpoorly@gmail.com>
2023-01-30 00:12:27 -0800

    Update Javascript-factory-objects.js

--- JS/Javascript-factory-objects.js
+++ JS/Javascript-factory-objects.js
@@ -10,3 +10,18 @@ const person = myFunction("steve")
 console.log(person.myName.name)// steve
 person.changeName() //executed changename() function, changing name to bob.
 console.log(person.myName.name) // bob
+
+/////////////////////////////////////////////GETTER FUNCTION EXAMPLE////////////////////////////////////////
+function Person(name, surname) {
+  const changeName = (newName) => name = newName
+
+  return {
+    get name() {return name}, 
+    surname,
+    changeName
+  }
+}
+
+let greg = new Person('Greg', 'Beasly')
+greg.changeName = 'Barent'
+console.log(greg.name) // 'Barent'! Now we can see the change!
